6a77b5f207b25bdca77fcb18e7c41ce1803656fc
faicanhas <faicanhas@gmail.com>
2018-01-11 05:29:14 +0700

    Add files via upload

---
+++ glance.py
@@ -0,0 +1,23 @@
+import sys
+import time
+import telepot
+from telepot.loop import MessageLoop
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print(content_type, chat_type, chat_id)
+
+    if content_type == 'text':
+        bot.sendMessage(chat_id, msg['text'])
+    elif content_type != 'text':
+        bot.sendMessage(chat_id, 'Maaf, harap hanya mengirimkan pesan teks saja.')
+
+TOKEN = '186043755:AAGeHYtpo7l31MlzwBmGO_ZewDahotofOas'
+
+bot = telepot.Bot(TOKEN)
+MessageLoop(bot, handle).run_as_thread()
+print ('Listening ...')
+
+# Keep the program running.
+while 1:
+    time.sleep(10)
